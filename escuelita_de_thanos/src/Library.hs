08bd13a82883cb0130452d986464e45045f17fb9
franco-creator <francolosasso10@gmail.com>
2024-05-29 17:00:27 -0300

    aca arranque a hacer el parcial de thanos en otro proyecto por que estaba todo mezclado y era medio lio

--- escuelita_de_thanos/src/Library.hs
+++ escuelita_de_thanos/src/Library.hs
@@ -10,10 +10,21 @@ data Personaje = Personaje {edad :: Number, energia :: Number, habilidades :: [S
 
 type Universo = [Personaje]
 
+reducirMitadPersonajes :: Universo -> Universo
+reducirMitadPersonajes unUniverso = take (div (length unUniverso) 2) unUniverso
+
+-- no puedo hacer aca pattern matching por que tengo que contar la cantidad de elementos de la lista
 chasquearUniverso :: Guantelete -> Universo -> Universo
+chasquearUniverso unGuantelete unUniverso
+  | length (gemas unGuantelete) == 6 && material unGuantelete == "uru" = reducirMitadPersonajes unUniverso
+  | otherwise = unUniverso
+
+-- funciona pero la hago mas eficiente
+{-chasquearUniverso :: Guantelete -> Universo -> Universo
 chasquearUniverso unGuantelete unUniverso
   | length (gemas unGuantelete) == 6 && material unGuantelete == "uru" = take (div (length unUniverso) 2) (unUniverso)
   | otherwise = unUniverso
+-}
 
 -- Parte 2
 
@@ -28,26 +39,39 @@ masHabilidades :: Personaje -> Bool
 -- masHabilidades unUniverso = length(habilidades unUniverso) > 1
 masHabilidades unUniverso = ((> 1) . length . habilidades) unUniverso
 
-filtrarPersonajes :: Universo -> Universo
-filtrarPersonajes unUniverso = filter masHabilidades unUniverso
-
-sumarEnergia :: Personaje -> Number -> Number
+-- era de como hice antes
+-- sumarEnergia :: Personaje -> Number -> Number
 -- sumarEnergia unPersonaje sumatoria = sumatoria + energia unPersonaje
-sumarEnergia unPersonaje sumatoria = ((+ sumatoria) . energia) unPersonaje
+-- sumarEnergia unPersonaje sumatoria = ((+ sumatoria) . energia) unPersonaje
 
+obtenerSoloEnergia :: Personaje -> Number
+obtenerSoloEnergia unPersonaje = energia unPersonaje
+
+-- necesito hacer primero algo que me filtre los que tienen mas de una habilidad, luego sacarles su energia y luego hacer un foldr para hacer la sumatoria y devolverlo
 energiaTotal :: Universo -> Number
-energiaTotal unUniverso = foldr (sumarEnergia) 0 (filtrarPersonajes unUniverso)
+energiaTotal unUniverso = (sum . map (obtenerSoloEnergia) . filter (masHabilidades)) unUniverso
+
+-- energiaTotal :: Universo -> Number
+-- energiaTotal unUniverso = foldr (sumarEnergia) 0 (filtrarPersonajes unUniverso)
 
 -- Parte 3
 
+-- era de como lo hice antes
 quitarEnergia :: Personaje -> Number -> Number
 quitarEnergia unPersonaje cuantoRestar = (energia unPersonaje) - cuantoRestar
 
--- no me funciono hacer la composicion con la resta de esta manera
+-- no me funciono hacer la composicion con la resta de esta manera con la de arriba funcion
 -- quitarEnergia unPersonaje cuantoRestar = ((- cuantoRestar) . energia) unPersonaje
 
+aumentarODisminuirEnergia :: Number -> Personaje -> Personaje
+aumentarODisminuirEnergia cuantoDebilitar unPersonaje = unPersonaje {energia = energia unPersonaje - cuantoDebilitar}
+
 mente :: Number -> Personaje -> Personaje
-mente unNumero unPersonaje = Personaje (edad unPersonaje) (quitarEnergia unPersonaje unNumero) (habilidades unPersonaje) (nombre unPersonaje) (planeta unPersonaje)
+mente cuantoDebilitar unPersonaje = aumentarODisminuirEnergia cuantoDebilitar unPersonaje
+
+-- funcionaba pero la mejoro
+-- mente :: Number -> Personaje -> Personaje
+-- mente unNumero unPersonaje = Personaje (edad unPersonaje) (quitarEnergia unPersonaje unNumero) (habilidades unPersonaje) (nombre unPersonaje) (planeta unPersonaje)
 
 habilidadesDiferentes :: String -> String -> Bool
 habilidadesDiferentes unaHabilidad habilidadLista = habilidadLista /= unaHabilidad
